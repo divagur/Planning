3200ff0674159963701b8d25e0e4ba150bffbb91
dzhuravlev <dzhuravlev@almazantey.ru>
2025-02-28 18:42:05 +0300

    Разработка

---
+++ DataLayer/BaseObjects/ConnectionParams.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planning.DataLayer
+{
+    public static class ConnectionParams
+    {
+        public static string ServerName;
+        public static string BaseName;
+        public static string UserName;
+        public static string Pwd;
+
+
+    }
+}
---
+++ DataLayer/ShipmentMain/ShipmentMainAdapter.cs
@@ -0,0 +1,25 @@
+﻿using Planning.Kernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planning.DataLayer
+{
+    public class ShipmentMainAdapter : IDataAdaper
+    {
+        public string Table => "";
+
+        public string GetSaveSql(EditState editState)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetSelectItemSql()
+        {
+           
+            return "SP_PL_MainQueryP";
+        }
+    }
+}
